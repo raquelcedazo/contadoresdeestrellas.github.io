ec7986c4f6d7c07bfe740e65b8434044dc1a401e
Esteban Gonzalez Guardia <egonzalez@ciclope.info>
2018-07-21 08:47:25 +0200

    modal actions added

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -9,3 +9,14 @@ $(window).scroll(function() {
     $('.top').removeClass("up");
   }
 });
+
+$('div.modal').on('show.bs.modal', function() {
+        var modal = this;
+        var hash = modal.id;
+        window.location.hash = hash;
+        window.onhashchange = function() {
+                if (!location.hash){
+                        $(modal).modal('hide');
+                }
+        }
+});
