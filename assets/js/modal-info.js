25c162aa3cb46786b42ab4d0555458628ca8187e
Miguel García-Mauriño <m.gtaboada@alumnos.upm.es>
2018-11-06 21:21:20 +0100

    Show video info

--- assets/js/modal-info.js
+++ assets/js/modal-info.js
@@ -9,27 +9,27 @@ function moreInfo() {
 }
 
 function showModal() {
-    modal.classList.add('show-modal')
-    overlay.classList.add('show-overlay')
+  modal.classList.add('show-modal');
+  overlay.classList.add('show-overlay');
 }
 
 function hideModal() {
-    modal.classList.toggle('show-modal')
-    overlay.classList.toggle('show-overlay')
+  modal.classList.toggle('show-modal');
+  overlay.classList.toggle('show-overlay');
 }
 
 function setMoreInfo() {
     const infoArr = apiInfo.moreInfo;
     infoArr.map(info => {
-        const li = document.createElement('li')
-        const title = document.createElement('span')
-        title.innerHTML = info.title + ': '
-        title.classList.add('title')
-        const value = document.createElement('span')
-        value.innerHTML = info.value
-        value.classList.add('value')
-        li.append(title)
-        li.append(value)
-        document.getElementById('moreInfoList').append(li)
-    })
+      const li = document.createElement('li');
+      const title = document.createElement('span');
+      title.innerHTML = info.title + ': ';
+      title.classList.add('title');
+      const value = document.createElement('span');
+      value.innerHTML = info.value;
+      value.classList.add('value');
+      li.append(title);
+      li.append(value);
+      document.getElementById('moreInfoList').append(li);
+    });
 }
